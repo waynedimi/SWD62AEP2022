bbce91b37cff16e26c074876a02765368764d3ed
waynedimi <wayne.dimech.20@gmail.com>
2022-11-14 20:27:33 +0100

    deleted classes

--- Domain/Models/Category.cs
+++ Domain/Models/Category.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Models
 {
-    class Category
+    public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
     }
 }
--- Domain/Models/Item.cs
+++ Domain/Models/Item.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,28 @@ namespace Domain.Models
 {
     //Domain will be a project referenced by all the other layers and will
     //      hold the data containers i.e. Models
+
+    //Note: Code first Approach
+
+    //EFCore is the module that will be used to automatically shape/model the database
     public class Item
     {
+        [Key] // These attributes are for database generation
         public int Id { get; set; }
+
+        [StringLength(100)]
+        [Required]
         public string Name { get; set; }
+
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
         public double Price { get; set; }
+
         public string Description { get; set; }
+
         public string PhotoPath { get; set; }
+
     }
 }
