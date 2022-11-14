7bfa9849386824e6c4a4e730d2f5e0cb4c772829
waynedimi <wayne.dimech.20@gmail.com>
2022-11-14 19:36:19 +0100

    initail commit

---
+++ Domain/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    //Domain will be a project referenced by all the other layers and will
+    //      hold the data containers i.e. Models
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
